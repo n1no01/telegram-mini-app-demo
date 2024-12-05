991405cb3d3665780c5ead47f04aea42da8074e7
Nino Herenda <90546090+n1no01@users.noreply.github.com>
2024-12-05 17:15:28 +0100

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,30 @@
+"use strict";
+const startBtn = document.getElementById('startBtn');
+const modal = document.getElementById('modal');
+const tonWalletBtn = document.getElementById('ton-connect');
+const websiteBtn = document.getElementById('websiteBtn');
+const groupBtn = document.getElementById('groupBtn');
+// Check if the elements exist before adding event listeners
+if (startBtn && modal && tonWalletBtn && websiteBtn && groupBtn) {
+    // Show modal on start button click
+    startBtn.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        modal.classList.add('displayFlex');
+        startBtn.classList.add('hidden');
+    });
+    // TON Wallet Connect
+    tonWalletBtn.addEventListener('click', () => {
+        alert('TON Wallet Connect clicked');
+    });
+    // Website Redirect
+    websiteBtn.addEventListener('click', () => {
+        window.location.href = 'https://rzlt.io/';
+    });
+    // Telegram Group Redirect
+    groupBtn.addEventListener('click', () => {
+        window.location.href = 'https://t.me/cbinsider';
+    });
+}
+else {
+    console.error('One or more elements not found');
+}
---
+++ script.ts
@@ -0,0 +1,32 @@
+const startBtn = document.getElementById('startBtn') as HTMLButtonElement;
+const modal = document.getElementById('modal') as HTMLDivElement;
+const tonWalletBtn = document.getElementById('ton-connect') as HTMLButtonElement;
+const websiteBtn = document.getElementById('websiteBtn') as HTMLButtonElement;
+const groupBtn = document.getElementById('groupBtn') as HTMLButtonElement;
+
+// Check if the elements exist before adding event listeners
+if (startBtn && modal && tonWalletBtn && websiteBtn && groupBtn) {
+    // Show modal on start button click
+    startBtn.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        modal.classList.add('displayFlex');
+        startBtn.classList.add('hidden');
+    });
+
+    // TON Wallet Connect
+    tonWalletBtn.addEventListener('click', () => {
+        alert('TON Wallet Connect clicked');
+    });
+
+    // Website Redirect
+    websiteBtn.addEventListener('click', () => {
+        window.location.href = 'https://rzlt.io/';
+    });
+
+    // Telegram Group Redirect
+    groupBtn.addEventListener('click', () => {
+        window.location.href = 'https://t.me/cbinsider';
+    });
+} else {
+    console.error('One or more elements not found');
+}
